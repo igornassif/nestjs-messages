4a223a80618efa9c9f92ef466660fbbc60336ef3
Igor Nassif <igor.nassif@gmail.com>
2025-02-05 16:57:23 -0300

    Apply Eslint

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,34 +1,44 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
-    @Get()
-    findAll(@Query() pagination: any){
-        const { limit = 10, offset = 0 } = pagination;
-        return `This endpoint returns all messages. Limit: ${limit}, offset: ${offset}`;
-    }
+  @Get()
+  findAll(@Query() pagination: any) {
+    const { limit = 10, offset = 0 } = pagination;
+    return `This endpoint returns all messages. Limit: ${limit}, offset: ${offset}`;
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string){        
-        return `Message id: ${id}`;
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Message id: ${id}`;
+  }
 
-    @Post()
-    create(@Body() body: any){        
-        return body;
-    }
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() body: any){
-        return {
-            id,
-            ...body
-        }
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return {
+      id,
+      ...body,
+    };
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string){
-        return `Deleted message id: ${id}`;
-    }
-
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Deleted message id: ${id}`;
+  }
 }
