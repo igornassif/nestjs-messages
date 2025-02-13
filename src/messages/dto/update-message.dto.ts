63539538254392533f13ac66afb2a3395b04cad1
Igor Nassif <igor.nassif@gmail.com>
2025-02-13 15:11:39 -0300

    Add basic validation

--- src/messages/dto/update-message.dto.ts
+++ src/messages/dto/update-message.dto.ts
@@ -1,5 +1,30 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class UpdateMessageDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(255)
+  @IsOptional()
   readonly text?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
+  @IsOptional()
   readonly from?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
+  @IsOptional()
   readonly to?: string;
 }
