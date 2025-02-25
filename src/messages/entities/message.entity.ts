6dd691ccacc2de255c83bdda06cdc67cab2463f9
Igor Nassif <igor.nassif@gmail.com>
2025-02-25 18:26:25 -0300

    Cascade delete/update on Person

--- src/messages/entities/message.entity.ts
+++ src/messages/entities/message.entity.ts
@@ -29,11 +29,11 @@ export class Message {
   @UpdateDateColumn()
   updatedAt?: Date;
 
-  @ManyToOne(() => Person)
+  @ManyToOne(() => Person, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
   @JoinColumn({ name: 'from' })
   from: Person;
 
-  @ManyToOne(() => Person)
+  @ManyToOne(() => Person, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
   @JoinColumn({ name: 'to' })
   to: Person;
 }
